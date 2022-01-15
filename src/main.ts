1f35eb9f45f49a4243391d12bccef2a672443f71
Adrian Zimmer <z.adrian1995@gmail.com>
2022-01-15 12:37:36 +0100

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,43 @@ interface GAInputType {
 
 const inputTypes = ['data_input', 'data_collection_input']
 
-function main (): void {
-  // Read Ga file
-  const gaWorkflowJson = JSON.parse(fs.readFileSync(process.argv[2]).toString())
-
-  // Specify inputs
+function extractGaInputs (gaFile: any): GAInputType[] {
   const gaInputs: GAInputType[] = []
 
-  for (const input in gaWorkflowJson.steps) {
-    const step = gaWorkflowJson.steps[input]
+  for (const input in gaFile.steps) {
+    const step = gaFile.steps[input]
     const stepType = step.type
     if (inputTypes.includes(stepType)) {
       gaInputs.push({ name: step.label, type: stepType })
     }
   }
+  return gaInputs
+}
 
-  // generate preprocessing step
-  const preprocessingStep =
-  new cwlTsAuto.CommandLineTool({
+function mapGaTypeToCommandInputParameterType (gaType: string): cwlTsAuto.CommandInputParameterProperties['type'] {
+  switch (gaType) {
+    case 'data_input': {
+      return cwlTsAuto.CWLType.FILE
+    }
+    case 'data_collection_input': {
+      return new cwlTsAuto.CommandInputArraySchema({ items: cwlTsAuto.CWLType.FILE, type: cwlTsAuto.enum_d062602be0b4b8fd33e69e29a841317b6ab665bc.ARRAY })
+    }
+    default: {
+      throw Error('Invalid input type: ' + gaType)
+    }
+  }
+}
+
+function createCommandInputParameter (input: GAInputType): cwlTsAuto.CommandInputParameter {
+  return new cwlTsAuto.CommandInputParameter({
+    inputBinding: new cwlTsAuto.CommandLineBinding({ prefix: '--file ' + input.name, shellQuote: false }),
+    label: input.name,
+    type: mapGaTypeToCommandInputParameterType(input.type)
+  })
+}
+
+function generatePreprocessingStepSkeleton (): cwlTsAuto.CommandLineTool {
+  const preprocessingStepSkeleton = new cwlTsAuto.CommandLineTool({
     baseCommand: 'cwl-galaxy-parser',
     cwlVersion: cwlTsAuto.CWLVersion.V1_2,
     requirements: [
@@ -36,22 +55,6 @@ function main (): void {
     inputs: [],
     outputs: []
   })
-
-  for (const input of gaInputs) {
-    let type: any
-    if (input.type === 'data_input') {
-      type = cwlTsAuto.CWLType.FILE
-    } else if (input.type === 'data_collection_input') {
-      type = new cwlTsAuto.CommandInputArraySchema({ items: cwlTsAuto.CWLType.FILE, type: cwlTsAuto.enum_d062602be0b4b8fd33e69e29a841317b6ab665bc.ARRAY })
-    }
-    const exampleInput =
-    new cwlTsAuto.CommandInputParameter({
-      inputBinding: new cwlTsAuto.CommandLineBinding({ prefix: '--file ' + input.name, shellQuote: false }),
-      label: input.name,
-      type: type
-    })
-    preprocessingStep.inputs.push(exampleInput)
-  }
 
   const paramFileOuput = new cwlTsAuto.CommandOutputParameter({
     type: cwlTsAuto.CWLType.FILE,
@@ -64,9 +67,32 @@ function main (): void {
     label: 'inputDataFolder',
     outputBinding: new cwlTsAuto.CommandOutputBinding({ glob: '$(runtime.outdir)' })
   })
-  preprocessingStep.outputs.push(paramFileOuput)
-  preprocessingStep.outputs.push(inputDatFolderOuput)
+  preprocessingStepSkeleton.outputs.push(paramFileOuput)
+  preprocessingStepSkeleton.outputs.push(inputDatFolderOuput)
+  return preprocessingStepSkeleton
+}
 
+function generatePreprocessingStep (gaInputs: GAInputType[]): cwlTsAuto.CommandLineTool {
+  const preprocessingStep = generatePreprocessingStepSkeleton()
+
+  for (const input of gaInputs) {
+    preprocessingStep.inputs.push(createCommandInputParameter(input))
+  }
+
+  return preprocessingStep
+}
+
+function main (): void {
+  // Read the galaxy Workflow file
+  const gaWorkflowJson = JSON.parse(fs.readFileSync(process.argv[2]).toString())
+
+  // Extract the input descriptions from the galaxy workflow
+  const gaInputs = extractGaInputs(gaWorkflowJson)
+
+  // Generate the cwl tool using the galaxy input descriptions
+  const preprocessingStep = generatePreprocessingStep(gaInputs)
+
+  // Print out the resulting cwl
   console.log(JSON.stringify(preprocessingStep.save(), null, 4))
 }
 
